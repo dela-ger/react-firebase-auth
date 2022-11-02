8d6b3e2afcabd04d7120485c3a5d1221c591406a
dela-ger <gdjameh@yahoo.com>
2022-11-02 13:02:15 +0000

    Add

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,7 +1,11 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import { Form, Button, Card } from 'react-bootstrap'
+import "bootstrap/dist/css/bootstrap.min.css"
 
 function Signup() {
+    const emailRef = useRef()
+    const passwordRef = useRef()
+    const passwordConfirmRef = useRef()
   return (
     <>
         <Card>
@@ -10,8 +14,17 @@ function Signup() {
                 <Form>
                     <Form.Group id='email'>
                         <Form.Label>Email</Form.Label>
-                        <Form.Control type='email' />
+                        <Form.Control type='email' ref={emailRef} required/>
                     </Form.Group>
+                    <Form.Group id='password'>
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control type='password' ref={passwordRef} required/>
+                    </Form.Group>
+                    <Form.Group id='password-confirmation'>
+                        <Form.Label>Password Confirmation</Form.Label>
+                        <Form.Control type='password' ref={passwordConfirmRef} required/>
+                    </Form.Group>
+                    <Button className='w-100 text-center mt-2' type='submit'>Submit</Button>
                 </Form>
             </Card.Body>
         </Card>
